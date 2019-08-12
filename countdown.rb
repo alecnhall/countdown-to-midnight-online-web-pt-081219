9138aa80cd1021c007026d4607137fd62d0d62ef
Alec Hall <alecnhall@gmail.com>
2019-08-12 22:36:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,13 +3,11 @@
 def countdown(num)
   while num > 0 
   puts "#{num} SECOND(S)!"
-  sleep(1)
-  end 
   num -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
 countdown_with_sleep(num)
-  sleep(num)
+
 end
